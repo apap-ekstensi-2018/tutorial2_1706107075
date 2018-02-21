f54b17e2a611701ec28aa77325b56f15a16a3d90
Winda Dumaria S <winda.dumaria@ruma.co.id>
2018-02-21 22:00:14 +0700

    add deliverables tutorial2

---
+++ project/demo/src/main/java/com/example/demo/controller/PageController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Optional;
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	 public String hello ()
+	 {
+		//return template to be used
+		return "hello";
+	 }
+	
+	@RequestMapping("/greeting")
+	public String greetings (@RequestParam(value = "name", required=false, defaultValue="dunia") String name, Model model)
+	{
+		model.addAttribute ("name", name);
+		return "greeting";
+	}
+	
+	@RequestMapping(value = {"/greetings","greetings/{name}"})
+	public String greetingPath (@PathVariable Optional<String> name, Model model)
+	{
+		if(name.isPresent()) {
+			model.addAttribute("name", name.get());
+		}else {
+			model.addAttribute ("name", "apap");
+		}
+		return "greeting";
+	}
+	
+	@RequestMapping("/perkalian")
+	public String perkalian (@RequestParam(value="a",required=false,defaultValue="0") int a, @RequestParam(value="b",required=false,defaultValue="0") int b, Model model)
+	{
+		model.addAttribute ("a", a);
+		model.addAttribute ("b", b);
+		model.addAttribute ("result", a*b);
+		return "perkalian";
+	}
+}
